c63be850091d6b004e28fe5f86a17842e4136672
Mohammad Arfat Karim <153488860+Arfatkarim@users.noreply.github.com>
2025-08-20 15:06:32 +0000

    1_dataTypes.jVariables

---
+++ Basic/1_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 1000500
+let accountEmail = "arafatkarim37@gmail"
+var accountPassword = "30330"
+accounCity = "Moheshkhali"
+
+// accountId = 2
+accountEmail = "hh@hh.com"
+accountPassword = "37293479"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword ])
+ 
