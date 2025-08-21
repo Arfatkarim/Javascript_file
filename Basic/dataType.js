1419e828120fdd5d6d7195757f32030bd0f533b4
Mohammad Arfat Karim <153488860+Arfatkarim@users.noreply.github.com>
2025-08-21 15:38:40 +0000

    datatype

---
+++ Basic/dataType.js
@@ -0,0 +1,21 @@
+// Primitive
+
+//7 types : String, number, boolean, null, undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const inLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// reference type (Non primitive)
+//Array, objects, function
+
+console.log(id == anotherId);
+
+
